f85ff005943fa5579efac7c603f85ed4f9591936
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-08-17 08:25:20 +0530

    u

--- Flutter App/first_flutter_project/lib/main.dart
+++ Flutter App/first_flutter_project/lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'news_app.dart'; // Import the other file
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.system,
+    
       home: Scaffold(
-        appBar: AppBar(title: const Text('News Grid')),
-        body: NewsGrid(),
+        appBar: AppBar(title: Text('Material Design Example')),
+        body: Center(child: Text('Hello, Material Design!')),
       ),
     );
   }
