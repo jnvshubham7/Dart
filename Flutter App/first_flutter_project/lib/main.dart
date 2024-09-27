d17be1db123c5daaf255ee12ba1fbb74096042a9
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-09-27 21:04:37 +0530

    u

--- Flutter App/first_flutter_project/lib/main.dart
+++ Flutter App/first_flutter_project/lib/main.dart
@@ -1,15 +1,40 @@
-import 'package:first_flutter_project/Camera_permission.dart'; // Adjust this import path
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double _progress = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CameraPermissionExample(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Animated Progress Bar'),
+        ),
+        body: Center(
+          child: TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0.0, end: 1.0),
+            duration: Duration(seconds: 2),
+            builder: (context, value, child) {
+              return Container(
+                width: 200,
+                height: 20,
+                child: LinearProgressIndicator(
+                  value: value,
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
